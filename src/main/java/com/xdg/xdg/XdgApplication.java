642125f8214e60addfa34abdd9d341c9f59261e2
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2018-03-07 22:39:12 -0300

    Bootstrap do projeto

---
+++ src/main/java/com/xdg/xdg/XdgApplication.java
@@ -0,0 +1,12 @@
+package com.xdg.xdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XdgApplication.class, args);
+	}
+}
