214e86a3fcbde66d3d9e44bcdf9515611bebe2f4
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2018-03-17 19:37:07 -0300

    Parte do bootstrapping

---
+++ src/main/java/com/xgh/infra/ExceptionManager.java
@@ -0,0 +1,26 @@
+package com.xgh.infra;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.xgh.exceptions.EntityNotFoundException;
+
+@ControllerAdvice
+public class ExceptionManager {
+
+	@ResponseStatus(code=HttpStatus.NOT_FOUND)
+	@ExceptionHandler({ EntityNotFoundException.class })
+	public @ResponseBody ErrorResponse handleException(EntityNotFoundException ex) {
+		return new ErrorResponse(2, "Entidade não encontrada!");
+	}
+
+	@ResponseStatus(code=HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler({ Exception.class })
+	public @ResponseBody ErrorResponse handleException(Exception ex) {
+		// TODO: logar
+		return new ErrorResponse(1, "Erro interno!");
+	}
+}
