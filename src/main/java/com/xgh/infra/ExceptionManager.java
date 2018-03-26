4a5473514d455d80e65727069dd48a5343b25528
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2018-03-26 00:14:48 -0300

    Alterando mensagem do log

--- src/main/java/com/xgh/infra/ExceptionManager.java
+++ src/main/java/com/xgh/infra/ExceptionManager.java
@@ -16,7 +16,7 @@ public class ExceptionManager {
 	@ResponseStatus(code=HttpStatus.NOT_FOUND)
 	@ExceptionHandler({ EntityNotFoundException.class })
 	public @ResponseBody ErrorResponse handleException(EntityNotFoundException ex) {
-		LogManager.getRootLogger().info(ex);
+		LogManager.getRootLogger().info("ExceptionManager", ex);
 		
 		return new ErrorResponse(2, "Entidade não encontrada");
 	}
@@ -24,7 +24,7 @@ public class ExceptionManager {
 	@ResponseStatus(code=HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler({ Exception.class })
 	public @ResponseBody ErrorResponse handleException(Exception ex) {
-		LogManager.getRootLogger().fatal(ex);
+		LogManager.getRootLogger().fatal("ExceptionManager", ex);
 		
 		return new ErrorResponse(1, "Erro interno não tratado");
 	}
@@ -32,7 +32,7 @@ public class ExceptionManager {
 	@ResponseStatus(code=HttpStatus.BAD_REQUEST)
 	@ExceptionHandler({ HttpMessageConversionException.class })
 	public @ResponseBody ErrorResponse handleException(HttpMessageConversionException ex) {
-		LogManager.getRootLogger().info(ex);
+		LogManager.getRootLogger().info("ExceptionManager", ex);
 		
 		return new ErrorResponse(3, ex.getCause().getCause().getMessage());
 	}
