ca678303df0b8ff653eb9fab507f794db6eebec3
João Paulo Trentini da Silva <joao.trentinii@gmail.com>
2018-03-27 22:52:35 -0300

    Criação do requisito Manter Proprietário

---
+++ src/test/java/com/xgh/valueobjects/CpfTest.java
@@ -0,0 +1,14 @@
+package com.xgh.valueobjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CpfTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
