c114393a8002b7d9e6beb82845bfc440e9682396
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2018-04-12 10:49:42 +0000

    Adicionando TODOs

--- src/main/java/com/xgh/XghApplication.java
+++ src/main/java/com/xgh/XghApplication.java
@@ -3,6 +3,8 @@ package com.xgh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO testar undertow
+// TODO ativar https no nginx
 @SpringBootApplication
 public class XghApplication {
 	public static void main(String[] args) {
