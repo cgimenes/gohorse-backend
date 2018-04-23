1491070363ad0fc8c379251471601f18ebe140bc
alexandre.requena <alexandre.requena.95@gmail.com>
2018-04-22 21:29:47 -0300

    Corrigido o InternmentQueryService

--- src/main/java/com/xgh/model/query/internment/InternmentQueryService.java
+++ src/main/java/com/xgh/model/query/internment/InternmentQueryService.java
@@ -1,25 +1,14 @@
 package com.xgh.model.query.internment;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
-import com.xgh.Constants;
+import com.xgh.infra.service.BasicQueryService;
 
 @Service
-public class InternmentQueryService {
+public class InternmentQueryService extends BasicQueryService<Internment, InternmentRepository>{
 	@Autowired
-	private InternmentRepository repository;
-
-	public Page<Internment> findAll(int page) {
-		PageRequest request = PageRequest.of(page, Constants.PAGE_SIZE.asInteger());
-		return repository.findAll(request);
+	protected InternmentQueryService(InternmentRepository repository) {
+		super(repository);	
 	}
-
-	public Internment findById(UUID id) {
-		return repository.getOne(id);
-	}
+	
 }
