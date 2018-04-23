e38660c58b05a293408182be0938bca947ead870
alexandre.requena <alexandre.requena.95@gmail.com>
2018-04-22 22:33:16 -0300

    Corrigidos os erros encontrados no CR

--- src/main/java/com/xgh/model/query/internment/InternmentQueryService.java
+++ src/main/java/com/xgh/model/query/internment/InternmentQueryService.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Service;
 import com.xgh.infra.service.BasicQueryService;
 
 @Service
-public class InternmentQueryService extends BasicQueryService<Internment, InternmentRepository>{
+public class InternmentQueryService extends BasicQueryService<Internment, InternmentRepository> {
 	@Autowired
 	protected InternmentQueryService(InternmentRepository repository) {
-		super(repository);	
+		super(repository);
 	}
-	
+
 }
