8d59b1a7ae85ba5e34f779fa34334aa1963ad217
André Fernando <andre.conceicao>
2018-05-03 13:14:49 -0300

    Alterado classe de tipo de distribuição para single value object, inserido algumas validações

--- src/main/java/com/xgh/model/command/valueobjects/DistributionType.java
+++ src/main/java/com/xgh/model/command/valueobjects/DistributionType.java
@@ -1,8 +1,8 @@
 package com.xgh.model.command.valueobjects;
 
-import com.xgh.buildingblocks.valueobject.ValueObject;
+import com.xgh.buildingblocks.valueobject.SingleValueObject;
 
-public class DistributionType implements ValueObject {
+public class DistributionType extends SingleValueObject<String> {
 
 	private static final long serialVersionUID = 6090488518103456243L;
 
@@ -11,7 +11,17 @@ public class DistributionType implements ValueObject {
     protected DistributionType() {}
 
     public DistributionType(String name) {
+        
+        if (name.length() < 1) {
+            throw new IllegalArgumentException("É necessário que o tipo da distribuição possua no mínimo 1 caracter");
+        }
+        if (name.length() > 100) {
+            throw new IllegalArgumentException("O limite máximo para o tamanho de um nome é de 100 caracteres");
+        }
+        
         this.name = name;
+        
+        
     }
 
     public String getName() {
