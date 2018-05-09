5238dc8229a5c062393a5e45e65df620e0132f9e
André Fernando <andre.conceicao>
2018-05-09 20:31:49 -0300

    Inserido utilização da BasicQueryController na classe SupplierQueryController.

--- src/main/java/com/xgh/model/query/supplier/SupplierQueryController.java
+++ src/main/java/com/xgh/model/query/supplier/SupplierQueryController.java
@@ -1,31 +1,15 @@
 package com.xgh.model.query.supplier;
 
-import java.util.UUID;
-
+import com.xgh.infra.controller.BasicQueryController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/suppliers")
-public class SupplierQueryController {
+public class SupplierQueryController extends BasicQueryController<Supplier, SupplierQueryService> {
     @Autowired
-    private SupplierQueryService service;
-
-    @GetMapping
-    public ResponseEntity<Page<Supplier>> findAll(@RequestParam(name = "page", defaultValue = "0") int page) {
-        Page<Supplier> suppliers = service.findAll(page);
-        return ResponseEntity.ok().body(suppliers);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Supplier> findById(@PathVariable UUID id) {
-        Supplier supplier = service.findById(id);
-        return ResponseEntity.ok().body(supplier);
+    public SupplierQueryController(SupplierQueryService service) {
+        super(service);
     }
 }
