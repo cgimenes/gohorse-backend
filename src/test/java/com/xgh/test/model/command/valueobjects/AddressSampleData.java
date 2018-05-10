4373ea693ff4c214bc1df4e2a54f74770abae95f
André Fernando <andre.conceicao>
2018-05-09 22:22:45 -0300

    Criado o sample do Supplier

--- src/test/java/com/xgh/test/model/command/valueobjects/AddressSampleData.java
+++ src/test/java/com/xgh/test/model/command/valueobjects/AddressSampleData.java
@@ -1,5 +1,15 @@
 package com.xgh.test.model.command.valueobjects;
 
-// TODO
+import com.xgh.model.command.valueobjects.Address;
+import com.xgh.model.command.valueobjects.PostalCode;
+
+import org.springframework.stereotype.Component;
+
+@Component("AddressSampleData")
 public class AddressSampleData {
+
+	public Address getSample() {
+		Address address = new Address(new PostalCode( "87005140", "Rua Tabaete", "Rua", "Jardim Tabaetê", "Maringá", "Paraná", "Brasil"), 33, "Apartamento 69");
+        return address;
+	}
 }
