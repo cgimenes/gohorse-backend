233eeb62faa52dc91d916779b0f3053f50c4efe0
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2018-06-14 00:11:56 -0300

    Adicionando configurações de CORS

--- src/main/java/com/xgh/XghApplication.java
+++ src/main/java/com/xgh/XghApplication.java
@@ -2,6 +2,10 @@ package com.xgh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // TODO testar undertow
 // TODO ativar https no nginx
@@ -9,5 +13,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XghApplication {
     public static void main(String[] args) {
         SpringApplication.run(XghApplication.class, args);
+    }
+
+    @Configuration
+    @EnableWebMvc
+    public static class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowCredentials(true);
+        }
     }
 }
