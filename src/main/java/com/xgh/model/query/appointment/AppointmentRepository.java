c1b61a9ee69a734be9b9cd88e7544b37c220dab3
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2018-08-18 00:11:22 -0300

    Terminando a listagem de consultas

--- src/main/java/com/xgh/model/query/appointment/AppointmentRepository.java
+++ src/main/java/com/xgh/model/query/appointment/AppointmentRepository.java
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AppointmentRepository extends BasicJpaRepository<Appointment, UUID> {
     default List<Appointment> findByDate(LocalDate date) {
-        return findByDateTimeBetween(date.atStartOfDay(), date.plusDays(1).atStartOfDay());
+        return findByDateTimeBetweenOrderByDateTimeAsc(date.atStartOfDay(), date.plusDays(1).atStartOfDay());
     }
 
     default List<Appointment> findDatesWithAppointmentByMonth(LocalDate date) {
         LocalDateTime month = date.withDayOfMonth(1).atStartOfDay();
-        return findByDateTimeBetween(month, month.plusMonths(1).withDayOfMonth(1).minusDays(1));
+        return findByDateTimeBetweenOrderByDateTimeAsc(month, month.plusMonths(1).withDayOfMonth(1).minusDays(1));
     }
 
-    List<Appointment> findByDateTimeBetween(LocalDateTime from, LocalDateTime to);
+    List<Appointment> findByDateTimeBetweenOrderByDateTimeAsc(LocalDateTime from, LocalDateTime to);
 }
