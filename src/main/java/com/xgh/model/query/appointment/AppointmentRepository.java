e5b6676813975b233219a3f2f0bdbd391c0e11c9
alexandre.requena <alexandre.requena.95@gmail.com>
2018-08-29 21:19:56 -0300

    fix: corrige consulta das consultas agendadas com marcação de 'deleted'

--- src/main/java/com/xgh/model/query/appointment/AppointmentRepository.java
+++ src/main/java/com/xgh/model/query/appointment/AppointmentRepository.java
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AppointmentRepository extends BasicJpaRepository<Appointment, UUID> {
     default List<Appointment> findByDate(LocalDate date) {
-        return findByDateTimeBetween(date.atStartOfDay(), date.plusDays(1).atStartOfDay());
+        return findByDeletedFalseAndDateTimeBetween(date.atStartOfDay(), date.plusDays(1).atStartOfDay());
     }
 
     default List<Appointment> findDatesWithAppointmentByMonth(LocalDate date) {
         LocalDateTime month = date.withDayOfMonth(1).atStartOfDay();
-        return findByDateTimeBetween(month, month.plusMonths(1).withDayOfMonth(1).minusDays(1));
+        return findByDeletedFalseAndDateTimeBetween(month, month.plusMonths(1).withDayOfMonth(1).minusDays(1));
     }
 
-    List<Appointment> findByDateTimeBetween(LocalDateTime from, LocalDateTime to);
+    List<Appointment> findByDeletedFalseAndDateTimeBetween(LocalDateTime from, LocalDateTime to);
 }
