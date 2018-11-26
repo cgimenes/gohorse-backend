d3ee6d215a527214e3672e792b8a8cd899a54a7d
André Fernando <andre.conceicao>
2018-11-26 21:51:52 -0200

    Corrigido para trazer somente enums não deletados

--- src/main/java/com/xgh/model/query/operational/enumerator/EnumeratorQueryService.java
+++ src/main/java/com/xgh/model/query/operational/enumerator/EnumeratorQueryService.java
@@ -1,14 +1,11 @@
 package com.xgh.model.query.operational.enumerator;
 
-import com.xgh.Constants;
 import com.xgh.infra.service.BasicQueryService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,9 +16,8 @@ public class EnumeratorQueryService extends BasicQueryService<Enumerator, Enumer
         super(repository);
     }
     
-    public Page<Enumerator> findByTypeContainingIgnoreCase(int page, String kind) {
-        PageRequest request = PageRequest.of(page, Constants.PAGE_SIZE.asInteger());
-        return this.repository.findByKindContainingIgnoreCase(request,kind);
+    public List<Enumerator> findByDeletedFalseAndKindContainingIgnoreCase(int page, String kind) {
+        return this.repository.findByDeletedFalseAndKindContainingIgnoreCase(kind);
     }
 
 	public ArrayList<EnumeratorGroup> findAllGroupingByKind() {
--- src/main/java/com/xgh/model/query/operational/enumerator/EnumeratorRepository.java
+++ src/main/java/com/xgh/model/query/operational/enumerator/EnumeratorRepository.java
@@ -1,6 +1,8 @@
 package com.xgh.model.query.operational.enumerator;
 
 import com.xgh.infra.repository.BasicJpaRepository;
+
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,5 +10,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EnumeratorRepository extends BasicJpaRepository<Enumerator, UUID> {
-    Page<Enumerator> findByKindContainingIgnoreCase(Pageable pageable, String kind);
+    List<Enumerator> findByDeletedFalseAndKindContainingIgnoreCase(String kind);
 }
